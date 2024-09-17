da20ad741ff56266d47425690973bbeb4c4bf233
Caio Hiroshi <caio.hiroshi4274@gmail.com>
2024-09-17 08:52:35 -0300

    Montagem da tela de splash

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            progressBar1 = new ProgressBar();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources._12_59_14_660_512;
+            pictureBox1.Location = new Point(408, 36);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(365, 325);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Showcard Gothic", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(75, 82);
+            label1.Name = "label1";
+            label1.Size = new Size(215, 36);
+            label1.TabIndex = 1;
+            label1.Text = "CARREGANDO";
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(2, 415);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(797, 33);
+            progressBar1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Historic", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(30, 212);
+            label2.Name = "label2";
+            label2.Size = new Size(289, 30);
+            label2.TabIndex = 3;
+            label2.Text = "A MELHOR CALCULADORA";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(progressBar1);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Calculadora3000";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private ProgressBar progressBar1;
+        private Label label2;
     }
 }
