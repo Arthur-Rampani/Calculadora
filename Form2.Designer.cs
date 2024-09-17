8400b9eebf04a02639c9513ebdc2760b1f596553
Arthur Rampani <arthurrampanicosta@gmail.com>
2024-09-17 14:45:16 -0300

    Design Calculadora

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -28,12 +28,254 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form2";
+            BttAdição = new Button();
+            BttSubtração = new Button();
+            BttMultiplicação = new Button();
+            BttDivisão = new Button();
+            Btt1 = new Button();
+            Btt2 = new Button();
+            Btt3 = new Button();
+            BttCLS = new Button();
+            Btt4 = new Button();
+            Btt5 = new Button();
+            Btt6 = new Button();
+            Btt7 = new Button();
+            Btt8 = new Button();
+            Btt9 = new Button();
+            BttIgual = new Button();
+            Btt0 = new Button();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // BttAdição
+            // 
+            BttAdição.BackColor = SystemColors.ActiveCaption;
+            BttAdição.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            BttAdição.Location = new Point(60, 153);
+            BttAdição.Name = "BttAdição";
+            BttAdição.Size = new Size(60, 60);
+            BttAdição.TabIndex = 0;
+            BttAdição.Text = "+";
+            BttAdição.UseVisualStyleBackColor = false;
+            // 
+            // BttSubtração
+            // 
+            BttSubtração.BackColor = SystemColors.ActiveCaption;
+            BttSubtração.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            BttSubtração.Location = new Point(126, 153);
+            BttSubtração.Name = "BttSubtração";
+            BttSubtração.Size = new Size(60, 60);
+            BttSubtração.TabIndex = 1;
+            BttSubtração.Text = "-";
+            BttSubtração.UseVisualStyleBackColor = false;
+            // 
+            // BttMultiplicação
+            // 
+            BttMultiplicação.BackColor = SystemColors.ActiveCaption;
+            BttMultiplicação.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            BttMultiplicação.Location = new Point(192, 153);
+            BttMultiplicação.Name = "BttMultiplicação";
+            BttMultiplicação.Size = new Size(60, 60);
+            BttMultiplicação.TabIndex = 2;
+            BttMultiplicação.Text = "*";
+            BttMultiplicação.UseVisualStyleBackColor = false;
+            // 
+            // BttDivisão
+            // 
+            BttDivisão.BackColor = SystemColors.ActiveCaption;
+            BttDivisão.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            BttDivisão.Location = new Point(258, 153);
+            BttDivisão.Name = "BttDivisão";
+            BttDivisão.Size = new Size(60, 60);
+            BttDivisão.TabIndex = 3;
+            BttDivisão.Text = "/";
+            BttDivisão.UseVisualStyleBackColor = false;
+            // 
+            // Btt1
+            // 
+            Btt1.BackColor = SystemColors.ActiveCaption;
+            Btt1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Btt1.Location = new Point(60, 219);
+            Btt1.Name = "Btt1";
+            Btt1.Size = new Size(60, 60);
+            Btt1.TabIndex = 4;
+            Btt1.Text = "1";
+            Btt1.UseVisualStyleBackColor = false;
+            // 
+            // Btt2
+            // 
+            Btt2.BackColor = SystemColors.ActiveCaption;
+            Btt2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Btt2.Location = new Point(126, 219);
+            Btt2.Name = "Btt2";
+            Btt2.Size = new Size(60, 60);
+            Btt2.TabIndex = 5;
+            Btt2.Text = "2";
+            Btt2.UseVisualStyleBackColor = false;
+            // 
+            // Btt3
+            // 
+            Btt3.BackColor = SystemColors.ActiveCaption;
+            Btt3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Btt3.Location = new Point(192, 219);
+            Btt3.Name = "Btt3";
+            Btt3.Size = new Size(60, 60);
+            Btt3.TabIndex = 6;
+            Btt3.Text = "3";
+            Btt3.UseVisualStyleBackColor = false;
+            // 
+            // BttCLS
+            // 
+            BttCLS.BackColor = SystemColors.ActiveCaption;
+            BttCLS.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            BttCLS.Location = new Point(258, 219);
+            BttCLS.Name = "BttCLS";
+            BttCLS.Size = new Size(60, 60);
+            BttCLS.TabIndex = 7;
+            BttCLS.Text = "CLS";
+            BttCLS.UseVisualStyleBackColor = false;
+            // 
+            // Btt4
+            // 
+            Btt4.BackColor = SystemColors.ActiveCaption;
+            Btt4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Btt4.Location = new Point(60, 285);
+            Btt4.Name = "Btt4";
+            Btt4.Size = new Size(60, 60);
+            Btt4.TabIndex = 8;
+            Btt4.Text = "4";
+            Btt4.UseVisualStyleBackColor = false;
+            // 
+            // Btt5
+            // 
+            Btt5.BackColor = SystemColors.ActiveCaption;
+            Btt5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Btt5.Location = new Point(126, 285);
+            Btt5.Name = "Btt5";
+            Btt5.Size = new Size(60, 60);
+            Btt5.TabIndex = 9;
+            Btt5.Text = "5";
+            Btt5.UseVisualStyleBackColor = false;
+            // 
+            // Btt6
+            // 
+            Btt6.BackColor = SystemColors.ActiveCaption;
+            Btt6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Btt6.Location = new Point(192, 285);
+            Btt6.Name = "Btt6";
+            Btt6.Size = new Size(60, 60);
+            Btt6.TabIndex = 10;
+            Btt6.Text = "6";
+            Btt6.UseVisualStyleBackColor = false;
+            // 
+            // Btt7
+            // 
+            Btt7.BackColor = SystemColors.ActiveCaption;
+            Btt7.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Btt7.Location = new Point(60, 351);
+            Btt7.Name = "Btt7";
+            Btt7.Size = new Size(60, 60);
+            Btt7.TabIndex = 11;
+            Btt7.Text = "7";
+            Btt7.UseVisualStyleBackColor = false;
+            // 
+            // Btt8
+            // 
+            Btt8.BackColor = SystemColors.ActiveCaption;
+            Btt8.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Btt8.Location = new Point(126, 351);
+            Btt8.Name = "Btt8";
+            Btt8.Size = new Size(60, 60);
+            Btt8.TabIndex = 12;
+            Btt8.Text = "8";
+            Btt8.UseVisualStyleBackColor = false;
+            // 
+            // Btt9
+            // 
+            Btt9.BackColor = SystemColors.ActiveCaption;
+            Btt9.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Btt9.Location = new Point(192, 351);
+            Btt9.Name = "Btt9";
+            Btt9.Size = new Size(60, 60);
+            Btt9.TabIndex = 13;
+            Btt9.Text = "9";
+            Btt9.UseVisualStyleBackColor = false;
+            // 
+            // BttIgual
+            // 
+            BttIgual.BackColor = SystemColors.ActiveCaption;
+            BttIgual.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            BttIgual.Location = new Point(258, 285);
+            BttIgual.Name = "BttIgual";
+            BttIgual.Size = new Size(60, 193);
+            BttIgual.TabIndex = 14;
+            BttIgual.Text = "=";
+            BttIgual.UseVisualStyleBackColor = false;
+            // 
+            // Btt0
+            // 
+            Btt0.BackColor = SystemColors.ActiveCaption;
+            Btt0.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Btt0.Location = new Point(60, 420);
+            Btt0.Name = "Btt0";
+            Btt0.Size = new Size(192, 58);
+            Btt0.TabIndex = 15;
+            Btt0.Text = "0";
+            Btt0.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(60, 114);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(258, 23);
+            textBox1.TabIndex = 16;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(383, 536);
+            Controls.Add(textBox1);
+            Controls.Add(Btt0);
+            Controls.Add(BttIgual);
+            Controls.Add(Btt9);
+            Controls.Add(Btt8);
+            Controls.Add(Btt7);
+            Controls.Add(Btt6);
+            Controls.Add(Btt5);
+            Controls.Add(Btt4);
+            Controls.Add(BttCLS);
+            Controls.Add(Btt3);
+            Controls.Add(Btt2);
+            Controls.Add(Btt1);
+            Controls.Add(BttDivisão);
+            Controls.Add(BttMultiplicação);
+            Controls.Add(BttSubtração);
+            Controls.Add(BttAdição);
+            Name = "Form2";
+            Text = "Form2";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button BttAdição;
+        private Button BttSubtração;
+        private Button BttMultiplicação;
+        private Button BttDivisão;
+        private Button Btt1;
+        private Button Btt2;
+        private Button Btt3;
+        private Button BttCLS;
+        private Button Btt4;
+        private Button Btt5;
+        private Button Btt6;
+        private Button Btt7;
+        private Button Btt8;
+        private Button Btt9;
+        private Button BttIgual;
+        private Button Btt0;
+        private TextBox textBox1;
     }
 }
